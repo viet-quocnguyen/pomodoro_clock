eeaf15e10b96c08e9b5fce41972805b14e865a31
qvnguyen <nguyenquocviet170898@gmail.com>
2019-02-25 23:12:50 -0500

    Updates

---
+++ precache-manifest.60784cc2ed26fed6f011fcd10f9a1e1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52a3a987187be3019720",
+    "url": "/pomodoro_clock/static/js/runtime~main.52a3a987.js"
+  },
+  {
+    "revision": "ac822f16703da7b26b07",
+    "url": "/pomodoro_clock/static/js/main.ac822f16.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/pomodoro_clock/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "ac822f16703da7b26b07",
+    "url": "/pomodoro_clock/static/css/main.3b7a84d4.chunk.css"
+  },
+  {
+    "revision": "9961236f4c292b848f450051aa37b77d",
+    "url": "/pomodoro_clock/index.html"
+  }
+];
